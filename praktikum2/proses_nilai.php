598454750ffbdaed061cafd02a5331443e18f8f3
Rahmi Fauziah Rambe <fauziahrahmi933@gmail.com>
2024-03-05 21:17:27 +0700

    praktikum1

--- praktikum2/proses_nilai.php
+++ praktikum2/proses_nilai.php
@@ -7,13 +7,29 @@ $uas = $_GET['uas'];
 $tugas = $_GET['tugas'];
 
 // hitung nilai total (30% uts + 35% uas + 35% tugas)
-$nilai_total = (30/100 * $uts) + (35/100 * $uas) + (35/100 * $tugas);
+$nilai_total = (30 / 100 * $uts) + (35 / 100 * $uas) + (35 / 100 * $tugas);
 
-if($nilai_total > 55) {
+if ($nilai_total > 55) {
     $status = 'lulus';
 } else {
     $status = 'tidak lulus';
 }
+
+if ($nilai_total >= 85 && $nilai_total <= 100) {
+    $grade = "A (Sangat baik)";
+} elseif ($nilai_total >= 70 && $nilai_total <= 84) {
+    $grade = "B (baik)";
+} elseif ($nilai_total >= 56 && $nilai_total <= 69) {
+    $grade = "C (Cukup)";
+} elseif ($nilai_total >= 36 && $nilai_total <= 55) {
+    $grade = "D (Kurang)";
+} elseif ($nilai_total >= 0 && $nilai_total <= 35) {
+    $grade = "E (Sangat Kurang)";
+} else {
+    $grade = "I (Tidak Nilai)";
+}
+
+
 
 // tampilkan hasil form
 echo "Nama Lengkap: $nama";
@@ -23,3 +39,4 @@ echo "<br> Nilai UAS: $uas";
 echo "<br> Nilai Tugas: $tugas";
 echo "<br> Nilai Total: $nilai_total";
 echo "<br> Status: $status";
+echo "<br> Grade: $grade";
